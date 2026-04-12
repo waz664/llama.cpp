925a4a0f19ceff22ff936eb2d9ce4f8a7727bfc5
abhinavuser <abhinavkumarv2101@gmail.com>
2026-04-12 11:27:58 +0530

    llama-bench: fix accumulated load_time in perf timings

--- tools/llama-bench/llama-bench.cpp
+++ tools/llama-bench/llama-bench.cpp
@@ -2269,6 +2269,7 @@ int main(int argc, char ** argv) {
         test t(inst, lmodel, ctx);
 
         llama_memory_clear(llama_get_memory(ctx), false);
+        llama_perf_context_reset(ctx);
 
         // cool off before the test
         if (params.delay) {
